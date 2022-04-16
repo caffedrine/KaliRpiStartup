1a05aeeeea448957da65060cff4a305899c359bc
caffedrine <bacchusstyle@yahoo.com>
2022-04-16 18:08:26 +0000

    update startup script to copy wireless networks

--- startup.sh
+++ startup.sh
@@ -10,7 +10,11 @@ function log
     printf "] $1\n"
 }
 
+printf "\n"
 log "RPI Startup execution start..."
+
+log "Copy wpa_supplicant from bash..."
+cp /etc/wpa_supplicant/wpa_supplicant.conf /media/NTFS/startup/wpa_supplicant.conf
 
 log "Executing startup.py..."
 python /media/NTFS/startup/startup.py
